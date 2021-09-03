0554114bf4e4467d979e6eccf75e22106765b65a
Jerry <shijierui@gmail.com>
2021-09-03 15:21:10 +0100

    Code for 3.2 pc

---
+++ pc/3.2 Hand Gesture Recognition/mediapipe-demo.py
@@ -0,0 +1,28 @@
+import utils
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_holistic = utils.mp_holistic
+mp_drawing = utils.mp_drawing
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = utils.mediapipe_detection(frame, holistic)
+        
+        utils.draw_hand_landmarks(image, results)
+
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ pc/3.2 Hand Gesture Recognition/trainModel.py
@@ -0,0 +1,42 @@
+import modelParams
+import cv2
+import numpy as np
+import os
+import time
+import sklearn
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+           
+actions = modelParams.actions
+sequence_length = modelParams.sequence_length
+DATA_PATH = os.path.join('MP_Data') 
+label_map = {label:num for num, label in enumerate(actions)}    
+sequences, labels = [], []
+for action in actions:
+    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+        
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,126)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=2)
+model.save('actionDetection')
+
+
