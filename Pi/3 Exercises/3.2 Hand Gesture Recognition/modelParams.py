2dcbdf1f2e9265f799cb432a72685effc65ac9be
Jerry <shijierui@gmail.com>
2021-09-05 12:12:39 +0100

    update code for Pi

---
+++ Pi/3 Exercises/3.2 Hand Gesture Recognition/modelParams.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Actions to recognize
+actions = np.array(['up', 'down', 'stop'])
+
+# Number of videos for each action
+no_sequences = 30
+
+# Number of frames for each video
+sequence_length = 30
