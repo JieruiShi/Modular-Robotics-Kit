b67d7495f2424469e0867c77baba1f40311fb12c
Jerry <shijierui@gmail.com>
2021-09-10 11:08:21 +0100

    generate tflite model method

--- pc/3.2 Hand Gesture Recognition/trainModel.py
+++ pc/3.2 Hand Gesture Recognition/trainModel.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+import tensorflow as tf
            
 actions = modelParams.actions
 sequence_length = modelParams.sequence_length
@@ -28,7 +29,7 @@ y = to_categorical(labels).astype(int)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
 model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(sequence_length,126)))
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,126)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
@@ -36,7 +37,11 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=2)
+model.fit(X_train, y_train, epochs=200)
 model.save('actionDetection')
 
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
 
+with open('actionDetection.tflite','wb') as f:
+    f.write(tflite_model)
